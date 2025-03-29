46dea4e06061f7c6787498bb527f179cdcb19334
shivamsahray <sahrayshivam@gmail.com>
2025-03-29 14:55:50 +0530

    home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,8 +13,10 @@ function App() {
 
   return (
     <>
+      
       <BrowserRouter>
         <Routes>
+        <Route path='/' element={<Signup />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/signin' element={<Signin />} />
           <Route path='/blog/:id' element={<Blog />} />
